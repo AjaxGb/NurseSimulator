03d1343f5175cd2fd11ffc499a1c77f32373ff91
ghwlanglois <ghwlanglois@gmail.com>
2016-12-05 14:19:44 -0500

    added navmesh obstacles to all walls
    also fully enclosed parking lot

---
+++ Assets/Scripts/SpawnPatient.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPatient : MonoBehaviour {
+    public GameObject NPC;
+    public Transform[] SpawnPoints;
+    public float SpawnTime = 3f;
+    public GameObject[] patients;
+
+    void Start() {
+        InvokeRepeating("Spawn", SpawnTime, SpawnTime);
+    }
+    
+    void Spawn () {
+        int SpawnPointIndex = Random.Range(0, SpawnPoints.Length);
+        Instantiate(NPC, SpawnPoints[SpawnPointIndex].position, SpawnPoints[SpawnPointIndex].rotation);
+    }
+}
