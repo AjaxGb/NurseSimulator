00a426a57f7c7b09709dc5506ee17d462ca6f6d4
AjaxGb <ajaxgb@gmail.com>
2016-12-07 13:40:47 -0500

    Add trashcan script

---
+++ Assets/Scripts/Trashcan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class Trashcan : MonoBehaviour, IMouseOverUI {
+	public void ShowUI(Transform parent, Camera camera) {}
+	public void UpdateUI(Camera camera, Vector3 point) {}
+	public void HideUI() {}
+
+	public void OnClick(int button = 0) {
+		Player.inst.inventory.Clear();
+	}
+}
