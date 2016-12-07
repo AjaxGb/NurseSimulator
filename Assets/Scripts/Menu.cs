be13d2e57779f734180b9db7a83e988fb5503bed
Rikusaber <huange2@rpi.edu>
2016-12-07 12:04:29 -0500

    Title Screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
